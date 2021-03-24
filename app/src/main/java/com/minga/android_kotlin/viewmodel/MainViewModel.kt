80a9afdae18c7d3e8f43036410d670369abc3f81
Mingaleev <art.mingaleev@yandex.ru>
2021-03-24 19:37:20 +0300

    Реализовал ViewModel

--- app/src/main/java/com/minga/android_kotlin/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/minga/android_kotlin/viewmodel/MainViewModel.kt
@@ -1,7 +1,30 @@
 package com.minga.android_kotlin.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.minga.android_kotlin.model.Repository
+import com.minga.android_kotlin.model.RepositoryImpl
+import java.lang.Thread.sleep
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(
+    private val liveDataToObserve: MutableLiveData<AppState> = MutableLiveData(),
+    private val repositoryImpl: Repository = RepositoryImpl()
+) : ViewModel() {
+
+    fun getLiveData(): LiveData<AppState> = liveDataToObserve
+
+    fun getWeatherFromLocalSource(){
+        getDataFromLocalSource()
+    }
+
+    private fun getDataFromLocalSource(){
+        liveDataToObserve.value = AppState.Loading
+        Thread{
+            sleep(1000)
+            val data = repositoryImpl.getWeatherFromLocal()
+            liveDataToObserve.postValue(AppState.Success(data))
+        }
+    }
+
 }
