957981aefb5d78601eca492a68a8e9166ba10adf
Mingaleev <art.mingaleev@yandex.ru>
2021-03-31 20:52:28 +0300

    Переименовал MainFragment.kt to DetailsFragment.kt и xml файл Создал новый fragment_main.xml для списка городов и xml recycler'а

---
+++ app/src/main/java/com/minga/android_kotlin/view/details/DetailsFragment.kt
@@ -0,0 +1,71 @@
+package com.minga.android_kotlin.view.details
+
+import androidx.fragment.app.Fragment
+
+class DetailsFragment : Fragment() {
+
+//    companion object {
+//        fun newInstance() = DetailsFragment()
+//    }
+//
+//    private var _binding: FragmentDetailsBinding? = null
+//
+//    private val binding get() = _binding!!
+//
+//    private lateinit var viewModel: MainViewModel
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+//        return binding.root
+//    }
+//
+//    override fun onDestroyView() {
+//        super.onDestroyView()
+//        _binding = null
+//    }
+//
+//    override fun onActivityCreated(savedInstanceState: Bundle?) {
+//        super.onActivityCreated(savedInstanceState)
+//
+//        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+//
+//        viewModel
+//            .getLiveData()
+//            .observe(
+//                viewLifecycleOwner,
+//                Observer { renderData(it) }
+//            )
+//
+//        viewModel.getWeatherFromLocalSource()
+//    }
+//
+//    private fun renderData(appState: AppState) = when (appState) {
+//        is AppState.Success -> {
+//            val weatherData = appState.weatherData
+//            binding.loadingLayout.visibility = View.GONE
+//            setData(weatherData)
+//        }
+//
+//        is AppState.Loading -> {
+//            binding.loadingLayout.visibility = View.VISIBLE
+//        }
+//
+//        is AppState.Error -> {
+//            binding.loadingLayout.visibility = View.GONE
+//            Snackbar.make(binding.main, "Ошибка", Snackbar.LENGTH_INDEFINITE)
+//                .setAction("Повторить") { viewModel.getWeatherFromLocalSource() }
+//                .show()
+//        }
+//    }
+//
+//    private fun setData(weatherData: Weather) {
+//        binding.tvCity.text = weatherData.city.cityName
+//        binding.tvCondition.text = weatherData.condition
+//        binding.tvTemperatureNow.text = weatherData.temperature.toString()
+//        binding.tvFeelsLike.text = weatherData.feels_like.toString()
+//    }
+}
--- app/src/main/java/com/minga/android_kotlin/view/main/MainActivity.kt
+++ app/src/main/java/com/minga/android_kotlin/view/main/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.minga.android_kotlin.view
+package com.minga.android_kotlin.view.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.minga.android_kotlin.R
+import com.minga.android_kotlin.view.details.DetailsFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
+                .replace(R.id.container, DetailsFragment.newInstance())
                 .commitNow()
         }
     }
